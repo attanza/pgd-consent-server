3e2c2e54c964d5da9d33ec07996c33178ac5d57a
DaniDani <dani.vendor.sapti@pegadaian.co.id>
2022-06-30 00:09:45 +0700

    linting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { CheckListModule } from './check-list/check-list.module';
 import { ConsentModule } from './consent/consent.module';
 import { DatabaseModule } from './database/database.module';
-import { RolesGuard } from './shared/guards/roles.guard';
 import { TermModule } from './term/term.module';
 import { UserModule } from './user/user.module';
 
