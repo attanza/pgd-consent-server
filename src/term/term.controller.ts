714e049145e15f23ba8670d4cb7d1b03f4290965
DaniDani <dani.vendor.sapti@pegadaian.co.id>
2022-07-01 11:35:39 +0700

    term audit

--- src/term/term.controller.ts
+++ src/term/term.controller.ts
@@ -1,4 +1,15 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Query, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { MongoIdPipe } from '../shared/pipes/mongoId.pipe';
 
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
@@ -12,15 +23,22 @@ import {
 } from '../utils/response-parser';
 import { CreateTermDto, UpdateTermDto } from './term.dto';
 import { TermService } from './term.service';
-import { RolesGuard } from 'src/shared/guards/roles.guard';
-import { Roles } from 'src/shared/guards/roles.decorator';
-import { EUserRole } from 'src/shared/interfaces/user-role.enum';
+import { RolesGuard } from '../shared/guards/roles.guard';
+import { Roles } from '../shared/guards/roles.decorator';
+import { EUserRole } from '../shared/interfaces/user-role.enum';
+import { IRequest } from '../shared/interfaces/request.interface';
+import { AuditTrailsService } from 'src/audit-trails/audit-trails.service';
+import { EResourceAction } from '../shared/interfaces/resource-action';
+import { generateAuditData } from '../utils/generate-audit-data';
 
 @UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('terms')
 export class TermController {
   private resource = 'Term';
-  constructor(private readonly service: TermService) {}
+  constructor(
+    private readonly service: TermService,
+    private readonly auditService: AuditTrailsService,
+  ) {}
 
   @Get()
   async paginate(@Query() query: ResourcePaginationPipe) {
@@ -30,11 +48,13 @@ export class TermController {
 
   @Post()
   @Roles(EUserRole.ADMIN, EUserRole.EDITOR)
-  async create(@Body() data: CreateTermDto) {
+  async create(@Body() data: CreateTermDto, @Req() req: IRequest) {
     if (data.checkLists) {
       await this.service.checkListExists(data.checkLists);
     }
     const result = await this.service.create(data, ['title']);
+    const auditData = generateAuditData(req, EResourceAction.CREATE, this.resource, result);
+    this.auditService.auditTrail(auditData);
     return responseCreate(this.resource, result);
   }
 
@@ -49,18 +69,24 @@ export class TermController {
 
   @Put(':id')
   @Roles(EUserRole.ADMIN, EUserRole.EDITOR)
-  async update(@Param() { id }: MongoIdPipe, @Body() data: UpdateTermDto) {
+  async update(@Param() { id }: MongoIdPipe, @Body() data: UpdateTermDto, @Req() req: IRequest) {
+    const found = await this.service.getById(id);
     if (data.checkLists) {
       await this.service.checkListExists(data.checkLists);
     }
     const result = await this.service.update(id, data, ['title']);
+    const auditData = generateAuditData(req, EResourceAction.UPDATE, this.resource, result, found);
+    this.auditService.auditTrail(auditData);
     return responseUpdate(this.resource, result);
   }
 
   @Delete(':id')
   @Roles(EUserRole.ADMIN)
-  async destroy(@Param() { id }: MongoIdPipe) {
+  async destroy(@Param() { id }: MongoIdPipe, @Req() req: IRequest) {
+    const found = await this.service.getById(id);
     await this.service.delete(id);
+    const auditData = generateAuditData(req, EResourceAction.DELETE, this.resource, {}, found);
+    this.auditService.auditTrail(auditData);
     return responseDelete(this.resource);
   }
 }
